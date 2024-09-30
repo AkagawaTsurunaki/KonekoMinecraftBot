e916ed25f48a5b0122a42191aafd0b58aee1ad2e
AkagawaTsurunaki <965145619@qq.com>
2024-09-30 12:46:55 +0800

    feat: 状态机注册和跟随技能

---
+++ fsm/fsm.ts
@@ -0,0 +1,14 @@
+export abstract class State {
+    public env: any
+    public name: string
+    public nextStates: Array<State>
+
+    abstract cond()
+    abstract updateEnv(env: any)
+    abstract takeAction()
+
+}
+
+export abstract class FSM {
+
+}

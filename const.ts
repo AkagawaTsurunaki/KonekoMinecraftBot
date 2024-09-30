d9a4babe5e5df8514a380042971423083a24910b
AkagawaTsurunaki <965145619@qq.com>
2024-09-30 13:16:05 +0800

    feat: 机器人基础配置常量

---
+++ const.ts
@@ -0,0 +1,8 @@
+export const botOption = {
+    host: "127.0.0.1", // 主机地址
+    port: 25565, // 端口地址
+    username: "Koneko",
+    version: "1.20.1"
+}
+
+export const masterName: string = "Akagawa"

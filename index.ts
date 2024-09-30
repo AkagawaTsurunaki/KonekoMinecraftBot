afc58692cc3b46b6554bad7da6a67f5bfd4e3b39
AkagawaTsurunaki <965145619@qq.com>
2024-09-30 14:53:35 +0800

    fix: 修复了死区依赖

--- index.ts
+++ index.ts
@@ -3,7 +3,10 @@ import {createBot} from "mineflayer";
 import {log} from "./utils/log";
 import {pathfinder} from "mineflayer-pathfinder";
 import {plugin as pvp} from "mineflayer-pvp";
-import {KonekoFsm} from "./fsm/fsm";
+import {FSM} from "./fsm/fsm";
+import {IdleState} from "./fsm/idleState";
+import {FollowPlayerState} from "./fsm/followPlayerState";
+import {AttackHostileState} from "./fsm/attackHostileState";
 // import {loader as autoeat} from "mineflayer-auto-eat"
 
 export const bot = createBot(botOption)
@@ -12,6 +15,38 @@ bot.loadPlugin(pvp)
 // bot.loadPlugin(autoeat)
 
 log(`登录于 ${botOption.host}:${botOption.port}`)
+export class KonekoFsm extends FSM {
+    public update() {
+        try {
+            if (this.curState.cond()) {
+                this.curState.takeAction()
+            } else {
+                for (let nextState of this.curState.nextStates) {
+                    this.curState = nextState
+                }
+            }
+        } catch (e) {
+            // 如果出现 TypeError: curState.nextStates is not iterable，请检查您的状态机环路是否存在 null 节点。
+            throw e
+        }
+
+        log(`FSM 当前：${this.curState.name}`)
+    }
+
+    protected register() {
+        // 状态机注册
+        const idleState = new IdleState()
+        const followPlayerState = new FollowPlayerState()
+        const attackHostile = new AttackHostileState()
+
+        idleState.nextStates = [followPlayerState, attackHostile]
+        followPlayerState.nextStates = [idleState]
+        attackHostile.nextStates = [followPlayerState]
+
+        this.curState = idleState
+    }
+
+}
 
 const konekoFsm = new KonekoFsm()
 

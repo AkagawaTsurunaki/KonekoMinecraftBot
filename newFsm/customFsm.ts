1150b0a2a5641dedbd1c453598430d6161e78ecb
AkagawaTsurunaki <965145619@qq.com>
2024-10-11 13:28:22 +0800

    feat: 注册状态机制

--- newFsm/customFsm.ts
+++ newFsm/customFsm.ts
@@ -1,15 +1,18 @@
 import {IdleState} from "./states/idleState";
 import {AttackHostilesState} from "./states/attackHostilesState";
-import {AutoFiniteStateMachine, State} from "./fsm";
+import {AutoFiniteStateMachine} from "./fsm";
 
 export class CustomFsm extends AutoFiniteStateMachine {
 
-    idleState: State | null = null
-    attackHostilesState: State | null = null
+    private idleState: IdleState | null = null
+    private attackHostilesState: AttackHostilesState | null = null
 
     init() {
         this.idleState = new IdleState()
         this.attackHostilesState = new AttackHostilesState()
+
+        this.allStates[this.idleState.id] = this.idleState
+        this.allStates[this.attackHostilesState.id] = this.attackHostilesState
 
         this.idleState.nextStates = [this.attackHostilesState]
         this.attackHostilesState.nextStates = [this.idleState]

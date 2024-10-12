26c87efcb10ff9565019c5d0f3f190bbe1aca96b
AkagawaTsurunaki <965145619@qq.com>
2024-10-12 10:27:29 +0800

    feat: git add const.ts decorator.ts mermaid.ts

---
+++ src/common/const.ts
@@ -0,0 +1,18 @@
+export const botOption = {
+    host: "127.0.0.1", // 主机地址
+    port: 25565, // 端口地址
+    username: "Koneko",
+    version: "1.20.1"
+}
+
+export const masterName: string = "Akagawa"
+
+export const BEGINNING_OF_SUNSET: number = 12000
+export const BEGINNING_OF_NIGHT: number = 13000
+export const BEGINNING_OF_SUNRISE: number = 23000
+export const BEGINNING_OF_DAY: number = 24000
+
+export const corpsNameList: string[] = ['wheat', 'potatoes', 'carrots', 'beetroots']
+export const woodNameList: string[] = ['cherry_log', 'oak_log', 'dark_oak_log', 'spruce_log', 'birch_log', 'jungle_log', 'acacia_log', 'mangrove_log']
+export const axeNameList: string[] = ['diamond_axe', 'iron_axe', 'stone_axe', 'wooden_axe', 'golden_axe', 'netherite_axe']
+export const entityTypes = ['player', 'mob', 'object', 'global', 'orb', 'projectile', 'hostile', 'other']

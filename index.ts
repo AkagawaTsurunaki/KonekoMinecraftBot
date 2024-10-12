6ff1df4649adb83f6b61504951bcdb9761b06c15
AkagawaTsurunaki <965145619@qq.com>
2024-10-12 13:52:44 +0800

    refactor: Rename variables

--- index.ts
+++ index.ts
@@ -5,8 +5,7 @@ import {Vec3} from "vec3";
 import {getLogger} from "./src/utils/log";
 import {botOption, masterName} from "./src/common/const";
 import {startSecondEvent} from "./src/events/secondEvent";
-import {CustomFsm} from "./src/newFsm/customFsm";
-import {MermaidGenerator} from "./src/common/mermaid";
+import {CustomFSM} from "./src/newFsm/impl/customFSM";
 
 
 const logger = getLogger("index")
@@ -22,20 +21,40 @@ logger.info(`Login at ${botOption.host}:${botOption.port}`)
 startSecondEvent()
 
 logger.info(`Finite state machine initializing...`)
-const newFsm = new CustomFsm()
+const fsm = new CustomFSM()
 
 bot.on("spawn", () => {
     bot.chat(`Koneko 正在测试中……`)
-    newFsm.init()
-    newFsm.start()
+    fsm.init()
+    fsm.start()
     logger.info(`Finite state machine started.`)
-    new MermaidGenerator().generate(newFsm)
+    // new MermaidGenerator().generate(newFsm)
 });
 
 
 bot.on("chat", async (username, message, translate, jsonMsg) => {
     if (username === masterName && message === "quit") {
         bot.quit(`${masterName} asked you to quit.`)
+    } else if (username === masterName && message === "test") {
+        // const vec3s = bot.findBlocks({
+        //     point: bot.entity.position,
+        //     maxDistance: 3,
+        //     count: 5,
+        //     matching: block => block.skyLight < 10}
+        // );
+        // logger.debug(vec3s)
+        // const blockAt = bot.blockAt(bot.entity.position.offset(0, -1, 0));
+        // if (!blockAt) {
+        //     logger.debug("null")
+        //     return
+        // }
+        // logger.debug([blockAt.light, blockAt.skyLight])
+        // bot.findBlocks({point: bot.entity.position, maxDistance: 16, count: 2000, matching: block => block.skyLight <= 10}).forEach(
+        //     block => {
+        //         const cmd = `/setblock ${block.x} ${block.y} ${block.z} minecraft:white_wool`;
+        //         bot.chat(cmd)
+        //     }
+        // )
     }
 })
 

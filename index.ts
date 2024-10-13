011e3f8a6394c765eb88cc8518db3bcdbe450aaf
AkagawaTsurunaki <965145619@qq.com>
2024-10-13 13:44:16 +0800

    refactor: Koneko 的初始化放在同一个函数中，且避免重复加载导致

--- index.ts
+++ index.ts
@@ -5,55 +5,51 @@ import {Vec3} from "vec3";
 import {getLogger} from "./src/utils/logger";
 import {botOption, masterName} from "./src/common/const";
 import {startSecondEvent} from "./src/events/secondEvent";
-import {DamageType, startDamageEvent} from "./src/events/damageEvent";
-import {myEmitter} from "./src/events/extendedBotEvents";
-import {Entity} from "prismarine-entity";
-import { startBotDamageEvent } from "./src/events/botHurtEvent";
+import {startDamageEvent} from "./src/events/damageEvent";
+import {startBotDamageEvent} from "./src/events/botHurtEvent";
+import {CustomFSM} from "./src/newFsm/impl/customFSM";
 
 
 const logger = getLogger("index")
 export const bot = createBot(botOption)
-
-// 加载插件
-bot.loadPlugin(pathfinder)
-bot.loadPlugin(pvp)
-
 logger.info(`Login at ${botOption.host}:${botOption.port}`)
 
-// 启动自定义事件发射器
-startSecondEvent()
-startDamageEvent()
-startBotDamageEvent()
+function initKoneko() {
+    // Load all plugins.
+    bot.loadPlugin(pathfinder)
+    bot.loadPlugin(pvp)
+    logger.info(`All plugins loaded.`)
 
-logger.info(`Finite state machine initializing...`)
-// const fsm = new CustomFSM()
+    // Start custom event emitters.
+    startSecondEvent()
+    startDamageEvent()
+    startBotDamageEvent()
+    logger.info(`Extended event emitter started.`)
 
-bot.on("spawn", () => {
-    bot.chat(`Koneko 正在测试中……`)
-    // fsm.init()
-    // fsm.start()
+    // Start finite state machine.
+    const fsm = new CustomFSM()
+    fsm.init()
+    fsm.start()
     logger.info(`Finite state machine started.`)
     // new MermaidGenerator().generate(newFsm)
+
+    logger.info(`${bot.username} is running.`)
+}
+
+bot.once("spawn", () => {
+    initKoneko()
 });
 
 
 bot.on("chat", async (username, message, translate, jsonMsg) => {
     if (username === masterName && message === "quit") {
+        bot.chat("那我走喵~")
         bot.quit(`${masterName} asked you to quit.`)
     } else if (username === masterName && message === "test") {
 
     }
 })
 
-myEmitter.on("botDamageEvent", (botEntity: Entity,
-                             sourceType: DamageType,
-                             sourceCause: Entity | null,
-                             sourceDirect: Entity | null) => {
-    console.log(`${botEntity.username} got ${sourceType.name} damage by ${sourceDirect?.name} from ${sourceCause?.name}.`)
-    if (sourceType.name === "arrow") {
-        bot.chat("Ouch... Take care of your bow and arrows!!!")
-    }
-})
 
 bot.on("hardcodedSoundEffectHeard", async (soundId: number,
                                            soundCategory: string | number,

02caa739b9ef7d3b6d32c1b2a327e66ee633dfb4
AkagawaTsurunaki <965145619@qq.com>
2024-10-13 11:08:19 +0800

    feat: 实现 damageEvent

---
+++ src/events/botHurtEvent.ts
@@ -0,0 +1,10 @@
+import {bot} from "../../index";
+import {myEmitter} from "./extendedBotEvents";
+
+export function startBotDamageEvent() {
+    myEmitter.on("damageEvent", (entity, sourceType, sourceCause, sourceDirect) => {
+        if (entity.type === 'player' && entity.username === bot.username) {
+            myEmitter.emit("botDamageEvent", entity, sourceType, sourceCause, sourceDirect)
+        }
+    })
+}

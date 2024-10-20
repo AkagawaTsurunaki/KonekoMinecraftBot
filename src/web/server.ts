8d46c7f5dced7ec456d1cc03290192d654012747
AkagawaTsurunaki <965145619@qq.com>
2024-10-20 15:59:25 +0800

    refactor: Made class WebServer

--- src/web/server.ts
+++ src/web/server.ts
@@ -2,58 +2,65 @@ import * as http from 'http';
 import * as fs from 'fs';
 import WebSocket from 'ws';
 
-// 客户端连接列表
-const clients: Set<WebSocket> = new Set();
+export class WebServer {
+    private readonly hostname: string = '127.0.0.1';
+    private readonly port: number = 5689;
+    private readonly websocketPort: number = 5690
+    private readonly httpServer: http.Server<typeof http.IncomingMessage, typeof http.ServerResponse>
+    private readonly websocketServer: WebSocket.Server<typeof WebSocket, typeof http.IncomingMessage>
+    private readonly clients: Set<WebSocket> = new Set();
 
-export function startServer() {
-    const hostname = '127.0.0.1';
-    const port = 5689;
+    constructor() {
+        this.httpServer = this.createHttpServer()
+        this.websocketServer = this.createWebsocketServer()
+    }
 
-    const server = http.createServer((req, res) => {
-        if (req.url === '/stateDiagram.html') {
-            fs.readFile("resources/static/stateDiagram.html", (err, data) => {
-                if (err) {
-                    res.writeHead(404);
-                    res.end('File not found');
-                } else {
-                    res.writeHead(200, {'Content-Type': 'text/html'});
-                    res.end(data);
-                }
-            });
-        } else {
-            res.writeHead(404);
-            res.end('Not Found');
-        }
-    });
-
-    // 创建WebSocket服务器
-    const wss = new WebSocket.Server({port: 5690})
-
-
-    wss.on('connection', (ws: any) => {
-        clients.add(ws);
-        console.log("Connected")
-        console.log(clients.size);
-        ws.on('close', () => {
-            clients.delete(ws);
+    startServer() {
+        this.httpServer.listen(this.port, this.hostname, () => {
+            console.log(`Server running at http://${this.hostname}:${this.port}/`);
         });
-    });
+    }
 
+    private createHttpServer() {
+        return http.createServer((req, res) => {
+            if (req.url === '/stateDiagram.html') {
+                fs.readFile("resources/static/stateDiagram.html", (err, data) => {
+                    if (err) {
+                        res.writeHead(404);
+                        res.end('File not found');
+                    } else {
+                        res.writeHead(200, {'Content-Type': 'text/html'});
+                        res.end(data);
+                    }
+                });
+            } else {
+                res.writeHead(404);
+                res.end('Not Found');
+            }
+        });
+    }
 
-    server.listen(port, hostname, () => {
-        console.log(`Server running at http://${hostname}:${port}/`);
-    });
+    private createWebsocketServer() {
+        const wss = new WebSocket.Server({port: this.websocketPort})
+
+        wss.on('connection', (ws: any) => {
+            this.clients.add(ws);
+            console.log("Connected")
+            console.log(this.clients.size);
+            ws.on('close', () => {
+                this.clients.delete(ws);
+            });
+        });
+
+        return wss
+    }
+
+    public pushMessage(data: any) {
+        // 向所有客户端推送数据
+        this.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify(data));
+            }
+        })
+    }
 }
-
-// 假设这是你的数据更新函数
-function updateData() {
-    // 模拟数据更新
-    const newData = 'New data from server';
-    // 向所有客户端推送数据
-    clients.forEach(client => {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(newData);
-        }
-    })
-}
-

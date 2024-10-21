1ad07c7ab2cd84b201479cf3f8682477072ccab0
AkagawaTsurunaki <965145619@qq.com>
2024-10-21 10:20:26 +0800

    refactor: startListeners and destroyListeners to manage event listeners.

--- src/cognition/observeBlockCognition.ts
+++ src/cognition/observeBlockCognition.ts
@@ -3,6 +3,8 @@ import {getLogger} from "../util/logger";
 import {ExtendedVec3} from "../extension/extendedVec3";
 import {AbstractCognition} from "./abstractCognition";
 import {ExtendedBot} from "../extension/extendedBot";
+import {AutoListener} from "../extension/autoListener";
+import {Block} from "prismarine-block";
 
 const logger = getLogger("PlacedLavaBlockCognition")
 
@@ -13,7 +15,7 @@ const logger = getLogger("PlacedLavaBlockCognition")
  * The bot will update their records of the water blocks (updated).
  * The water blockUpdate event emitted before `ObserveBlockCognition` was created will be ignored.
  */
-export class ObserveBlockCognition extends AbstractCognition {
+export class ObserveBlockCognition extends AbstractCognition implements AutoListener {
 
     private readonly observeBlockName: string
     private updatedBlockPositions: Set<string>
@@ -22,33 +24,40 @@ export class ObserveBlockCognition extends AbstractCognition {
         super(bot)
         this.observeBlockName = observeBlockName
         this.updatedBlockPositions = new Set()
-        this.onListen()
     }
 
-    onListen() {
-        logger.info(`Listening for blockUpdate for ${this.observeBlockName}`)
-        this.bot.on("blockUpdate", (oldBlock, newBlock) => {
-            assert(oldBlock)
-            if (oldBlock.name !== this.observeBlockName && newBlock.name === this.observeBlockName) {
-                const position = newBlock.position
-                this.updatedBlockPositions.add(ExtendedVec3.of(position).toCommaSplitString())
-            }
-            if (oldBlock.name === this.observeBlockName && newBlock.name !== this.observeBlockName) {
-                const position = oldBlock.position;
-                this.updatedBlockPositions.delete(ExtendedVec3.of(position).toCommaSplitString())
-            }
-        })
+    private onBlockUpdate(oldBlock: Block | null, newBlock: Block) {
+        assert(oldBlock)
+        if (oldBlock.name !== this.observeBlockName && newBlock.name === this.observeBlockName) {
+            const position = newBlock.position
+            this.updatedBlockPositions.add(ExtendedVec3.of(position).toCommaSplitString())
+        }
+        if (oldBlock.name === this.observeBlockName && newBlock.name !== this.observeBlockName) {
+            const position = oldBlock.position;
+            this.updatedBlockPositions.delete(ExtendedVec3.of(position).toCommaSplitString())
+        }
+    }
 
-        this.bot.events.on("secondTick", () => {
-            this.updatedBlockPositions.forEach(posStr => {
-                const pos = ExtendedVec3.fromCommaSplitString(posStr)
-                const blockAt = this.bot.blockAt(pos);
-                assert(blockAt)
-                if (blockAt.name !== this.observeBlockName) {
-                    this.updatedBlockPositions.delete(posStr)
-                }
-            })
-            logger.debug(`Block "${this.observeBlockName}" updated to: ${this.updatedBlockPositions.size}`)
+    private onSecondTick() {
+        this.updatedBlockPositions.forEach(posStr => {
+            const pos = ExtendedVec3.fromCommaSplitString(posStr)
+            const blockAt = this.bot.blockAt(pos);
+            assert(blockAt)
+            if (blockAt.name !== this.observeBlockName) {
+                this.updatedBlockPositions.delete(posStr)
+            }
         })
+        logger.debug(`Block "${this.observeBlockName}" updated to: ${this.updatedBlockPositions.size}`)
+    }
+
+    public destroyListeners(): void {
+        this.bot.removeListener("blockUpdate", this.onBlockUpdate)
+        this.bot.events.removeListener("secondTick", this.onSecondTick)
+    }
+
+    public startListeners(): void {
+        logger.info(`Listening for blockUpdate for ${this.observeBlockName}`)
+        this.bot.on("blockUpdate", this.onBlockUpdate)
+        this.bot.events.on("secondTick", this.onSecondTick)
     }
 }
---
+++ src/extension/autoListener.ts
@@ -0,0 +1,5 @@
+export interface AutoListener {
+    startListeners(): void
+
+    destroyListeners(): void
+}

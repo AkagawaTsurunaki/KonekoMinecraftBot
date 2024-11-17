763adfead3aaa42ce46f89fe87593ddf5289acc4
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2024-11-17 16:42:25 +0800

    feat: ToolCall and Tool classes

---
+++ src/agent/toolCall.ts
@@ -0,0 +1,61 @@
+export class ToolCall {
+    id: string;
+    name: string;
+    args: Record<string, any>
+
+
+    constructor(id: string, name: string, args: Record<string, any>) {
+        this.id = id;
+        this.name = name;
+        this.args = args;
+    }
+}
+
+export class ParameterProperty {
+    description: string;
+    name: string;
+    type: string;
+    isRequired: boolean;
+
+
+    constructor(description: string, name: string, type: string, isRequired: boolean) {
+        this.description = description;
+        this.name = name;
+        this.type = type;
+        this.isRequired = isRequired;
+    }
+}
+
+export class Tool {
+    "type": string;
+    "function": {
+        name: string;
+        description: string;
+        parameters: {
+            properties: Map<string, { description: string, type: string }>,
+            required: Array<string>,
+            type: string,
+        }
+    }
+
+    constructor(name: string, description: string, parameters: Array<ParameterProperty>, parametersType: string) {
+        this.type = "function";
+        const properties = new Map();
+        const required = new Array<string>();
+        for (const parameter of parameters) {
+            if (parameter.isRequired)
+                properties.set(parameter.name, {description: parameter.description, type: parameter.type});
+            else
+                required.push(parameter.name);
+        }
+        this.function = {
+            name: name,
+            description: description,
+            parameters: {
+                properties: properties,
+                required:required,
+                type: parametersType
+            }
+        }
+    }
+}

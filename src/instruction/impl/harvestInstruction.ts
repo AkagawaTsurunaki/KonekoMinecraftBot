35acd81f97d061a1790790f1172ec7be6ee7db2b
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2024-11-18 23:35:43 +0800

    feat: Implement HarvestInstruction01

--- src/instruction/impl/harvestInstruction.ts
+++ src/instruction/impl/harvestInstruction.ts
@@ -1,7 +1,8 @@
-import {Instruction} from "../instruction";
+import {BaseInstruction, BaseInstructionInput, instruction, Instruction} from "../instruction";
 import {getStopFlag, setStopFlag} from "../../share/flags";
-import { ExtendedBot } from "../../extension/extendedBot";
+import {ExtendedBot} from "../../extension/extendedBot";
 import {instructionDoc} from "../../common/decorator/instructionDoc";
+import {paramMetadata} from "../../common/fieldMetadata";
 
 @instructionDoc({name: "Harvest Corps", description: "Ask bot to harvest."})
 export class HarvestInstruction extends Instruction {
@@ -13,4 +14,26 @@ export class HarvestInstruction extends Instruction {
             }
         });
     }
+}
+
+class HarvestInstructionInput implements BaseInstructionInput {
+    @paramMetadata("搜索作物的半径大小", false)
+    searchRadius: number = 64
+    @paramMetadata("最多采集多少个作物", false)
+    maxCount: number = 1000
+}
+
+@instruction
+export class HarvestInstruction01 extends BaseInstruction {
+    private bot: ExtendedBot;
+
+    constructor(bot: ExtendedBot) {
+        super("harvest", "收割附近的作物", HarvestInstructionInput);
+        this.bot = bot
+    }
+
+    async exe(input: HarvestInstructionInput) {
+        await this.bot.skills.farm.harvest(input.searchRadius, input.maxCount, 5, () => getStopFlag())
+    }
+
 }
